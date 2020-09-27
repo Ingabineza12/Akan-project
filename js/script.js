e3ad4244611b456ce938d56d1aa8f50ccf73f36e
Deborah Ingabire <ingabineza@gmail.com>
2020-09-27 12:14:11 +0200

    added valid day function

--- js/script.js
+++ js/script.js
@@ -49,66 +49,49 @@ function giveCentury() {
   }
   
   function validDay(){
-    var thirtyOneMonths = [1, 3, 5, 7, 8, 10, 12];
-    var monthNumber = parseInt(document.getElementById("month").value);
+
     var dayNumber = parseInt(document.getElementById("day").value);
-    var yearNumber = parseInt(document.getElementById("year").value);
-    var yearNumber = parseInt(document.getElementById("century").value);
-    var a = yearNumber % 100;
-    var b = yearNumber % 400;
-    var c = yearNumber % 4;
-    var d = thirtyOneMonths.includes(monthNumber);
-    if (monthNumber === 2 && dayNumber > 28 && a === 0 && b !== 0){
-      alert("Invalid day: The entered year, February had 28 days.");
+    if (dayNumber > 31 || dayNumber < 1) {
+      alert("Invalid day! enter between 1 and 31 days");
       return false;
     }
-    else if (monthNumber === 2 && dayNumber > 28 && c !== 0) {
-      alert("Invalid day: The entered year, February had 28 days.");
-      return false;
-    }
-    else if (!d && dayNumber > 30) {
-      alert("Invalid day: The selected month has 30 days");
-      return false;
-    }
-    else if (dayNumber > 31 || dayNumber < 1) {
-      alert("Invalid day: Months have a possible 1 to 31 days");
-      return false;
-    }
+    
   }
+
   function getName(){
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var weekDays = ['0', '1', '2', '3', '4', '5', '6'];
+    var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
     var cc = parseInt(document.getElementById("century").value.slice(0, 2));
     var yy = parseInt(document.getElementById("year").value.slice(2, 4));
     var dd = parseInt(document.getElementById("day").value);
     var mm = parseInt(document.getElementById("month").value);
     var day = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
-    var akanDay = days[day.toFixed()];
+    var weekDays = days[day.toFixed()];
     var gender = parseInt(document.getElementById("gender").value);
     if (gender == male) {
-      if (akanDay === 'Sunday') {
-        document.getElementById("output").value = "Sunday,Akan Name is: " + akanMale[0];
+      if (weekDays === '0') {
+        document.getElementById("output").value = "Sunday,Akan Name is: " + maleNames[0];
         return true;
       }
-      else if (akanDay === 'Monday') {
-        document.getElementById("output").value = "Monday,Akan Name is: "+ akanMale[1];
+      else if (weekDays === '1') {
+        document.getElementById("output").value = "Monday,Akan Name is: "+ maleNames[1];
         return true;
       }
-      else if (akanDay === 'Tuesday') {
-        document.getElementById("output").value = "Tuesday,Akan Name is: " + akanMale[2];
+      else if (weekDays === '2') {
+        document.getElementById("output").value = "Tuesday,Akan Name is: " + maleNames[2];
         return true;
       }
-      else if (akanDay === 'Wednesday') {
-        document.getElementById("output").value = "Wednesday,Akan Name is: " + akanMale[3];
+      else if (weekDays === '3') {
+        document.getElementById("output").value = "Wednesday,Akan Name is: " + maleNames[3];
         return true;
       }
-      else if (akanDay === 'Thursday') {
-        document.getElementById("output").value = "Thursday,Akan Name is: " + akanMale[4];
+      else if (weekDays === '4') {
+        document.getElementById("output").value = "Thursday,Akan Name is: " + maleNames[4];
         return true;
       }
-      else if (akanDay === 'Friday') {
-        document.getElementById("output").value = "Friday,Akan Name is: " + akanMale[5];
+      else if (weekDays === '5') {
+        document.getElementById("output").value = "Friday,Akan Name is: " + maleNames[5];
         return true;
       }
       else {
@@ -117,32 +100,32 @@ function giveCentury() {
       }
     }
       if (gender == female) {
-        if (akanDay === 'Sunday') {
-          document.getElementById("output").value = "sunday,Akan Name is: "+ akanFemale[0];
+        if (weekDays === '0') {
+          document.getElementById("output").value = "sunday,Akan Name is: "+ femaleNames[0];
           return true;
         }
-        else if (akanDay === 'Monday') {
-          document.getElementById("output").value = "Monday,Akan Name is: "+ akanFemale[1];
+        else if (weekDays === '1') {
+          document.getElementById("output").value = "Monday,Akan Name is: "+ femaleNames[1];
           return true;
         }
-        else if (akanDay === 'Tuesday') {
-          document.getElementById("output").value = "Tuesday,Akan Name is: " + akanFemale[2];
+        else if (weekDays === '2') {
+          document.getElementById("output").value = "Tuesday,Akan Name is: " + femaleNames[2];
           return true;
         }
-        else if (akanDay === 'Wednesday') {
-          document.getElementById("output").value = "Wednesday,Akan Name is: " + akanFemale[3];
+        else if (weekDays === '3') {
+          document.getElementById("output").value = "Wednesday,Akan Name is: " + femaleNames[3];
           return true;
         }
-        else if (akanDay === 'Thursday') {
-          document.getElementById("output").value = "Thursday,Akan Name is: "+ akanFemale[4];
+        else if (weekDays === '4') {
+          document.getElementById("output").value = "Thursday,Akan Name is: "+ femaleNames[4];
           return true;
         }
-        else if (akanDay === 'Friday') {
-          document.getElementById("output").value = "Friday,Akan Name is: " + akanFemale[5];
+        else if (weekDays === '5') {
+          document.getElementById("output").value = "Friday,Akan Name is: " + femaleNames[5];
           return true;
         }
         else {
-          document.getElementById("output").value ="Saturday,Akan Name is: " + akanFemale[6];
+          document.getElementById("output").value ="Saturday,Akan Name is: " + femaleNames[6];
           return true;
         }
       }
